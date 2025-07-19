45c62b0fa4ab5d53f79734a5b00cf6623092cec9
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-18 23:21:17 -0400

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,24 @@
+friends = ['tom', 'mark', 'steve', 'joe']
+          # 0       1       2        3
+print(friends[0])
+
+#prints out tom 
+#starts with 0 at left. -1 is far right
+
+
+
+friends = ['tom', 'mark', 'steve', 'joe']
+          # 0       1       2        3
+print(friends.index('steve'))
+
+#tells you what index number steve is located. In this case 2
+
+
+friends = ['tom', 'mark', 'steve', 'joe']
+          # 0       1       2        3
+friends.append('jim')
+print(friends)
+
+#add to the list
+
+
