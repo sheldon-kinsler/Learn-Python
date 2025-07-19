4ba9292a40f661f39c184b901c6ffc20deaf9baf
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-18 23:14:49 -0400

    Create CreateMadLibGame.py

---
+++ CreateMadLibGame.py
@@ -0,0 +1,8 @@
+color = input("Enter a color: ")
+verb = input("Enter a verb: ")
+celeb = input("Enter a celeb :")
+
+
+print("Roses are " + color)
+print("I " + verb + " to work")
+print("I look like " + celeb)
