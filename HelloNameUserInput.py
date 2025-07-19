3bc1817a4ebfca06426233b51804a6b756986471
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-18 23:16:13 -0400

    Update HelloNameUserInput.py

--- HelloNameUserInput.py
+++ HelloNameUserInput.py
@@ -1,2 +1,8 @@
 name = input("What is your name? ")
 print("Hello, " + name + "!")
+
+#more user input variables
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+print("Hello, " + name + "! " + "You are " + age)
