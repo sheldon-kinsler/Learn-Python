ae545401c09f18c928d546af88b0aaeb1c649ed7
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-18 23:25:43 -0400

    Create CreatingFunctions.py

---
+++ CreatingFunctions.py
@@ -0,0 +1,39 @@
+def sayhi():
+   print("Hello User")
+
+sayhi()
+
+#prints Hello User
+
+
+
+def sayhi():
+   name = input("What is your name? ")
+   print("Hello " + name)
+
+sayhi()
+
+#function with input variable inside
+
+
+
+def sayhi():
+  name = input("What is your name? ")
+  age = input("How old are you? ")
+  print("Hello " + name + ", you are " + age + " years old.")
+
+sayhi()
+
+#multiple input variables inside of a function
+
+
+
+isdog = True
+
+if isdog:
+   print("Just a baby")
+else:
+   print("not a baby")
+
+#functions with if
+
