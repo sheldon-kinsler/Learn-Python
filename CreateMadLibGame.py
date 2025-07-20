785972343ffad0d075526f2f1392fce22521ecdf
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 18:27:39 -0400

    Update CreateMadLibGame.py

--- CreateMadLibGame.py
+++ CreateMadLibGame.py
@@ -1,8 +1,8 @@
-color = input("Enter a color: ")
-verb = input("Enter a verb: ")
-celeb = input("Enter a celeb :")
+color = input("Enter a color: ")  #prompts user to enter color
+verb = input("Enter a verb: ")    #prompts user to enter verb
+celeb = input("Enter a celeb :")  #prompts user to enter celeb
 
 
-print("Roses are " + color)
-print("I " + verb + " to work")
-print("I look like " + celeb)
+print("Roses are " + color)      #displays string + user inputted color
+print("I " + verb + " to work")  #displays string + user inputted verb
+print("I look like " + celeb)    #displays string + user inputted celeb
