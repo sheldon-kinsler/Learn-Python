88e0199f15a51aec4c451aef47c52795f173d724
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 18:11:30 -0400

    Update CreateReceipt.py

--- CreateReceipt.py
+++ CreateReceipt.py
@@ -28,3 +28,22 @@ print(f"Item: {Item1} - ${Price1:.2f}")
 print(f"Item: {Item2} - ${Price2:.2f}")
 print(f"Total: ${total:.2f}")
 print(f"Thank you for shopping with us, {Name}!")
+
+
+#improvements to above code
+
+Name = input("Enter Your Name: ")
+Item1 = input("Enter 1st Item: ")
+Item1 = Item1.capitalize()  #capitalizes first letter of word and keeps the rest lowercase
+Price1 = float(input("Enter the Price: "))
+Item2 = input("Enter 2nd Item: ")
+Item2 = Item2.capitalize()   #capitalizes first letter of word and keeps the rest lowercase. If they enter "mIlK" it will show up as "Milk"
+Price2 = float(input("Enter the Price: "))
+total = Price1 + Price2
+
+print("Customer: " + Name)
+print(f"Item: {Item1} - ${Price1:.2f}")
+print(f"Item: {Item2} - ${Price2:.2f}")
+print(f"Total: ${total:.2f}")
+print(f"Thank you for shopping with us, {Name}!")
+
