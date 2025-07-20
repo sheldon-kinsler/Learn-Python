a5bc8827fb7a9c8a401c41ccfeab170b5a17f291
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 19:25:14 -0400

    Update CreatingFunctions.py

--- CreatingFunctions.py
+++ CreatingFunctions.py
@@ -1,19 +1,15 @@
-def sayhi():
-   print("Hello User")
+def sayhi():  #def is to define the function
+   print("Hello User")  #value that the function will print when called
 
-sayhi()
-
-#prints Hello User
+sayhi()    #prints Hello User
 
 
 
-def sayhi():
-   name = input("What is your name? ")
-   print("Hello " + name)
+def sayhi():     #def is to define the function
+   name = input("What is your name? ")   #prompts user to enter value for the name variable
+   print("Hello " + name)   #displays string "Hello" and the user entered variable, name
 
-sayhi()
-
-#function with input variable inside
+sayhi()   #function with input variable inside
 
 
 
@@ -22,18 +18,24 @@ def sayhi():
   age = input("How old are you? ")
   print("Hello " + name + ", you are " + age + " years old.")
 
-sayhi()
-
-#multiple input variables inside of a function
+sayhi()    #multiple input variables inside of a function
 
 
 
-isdog = True
+isdog = True   #setting the variable to true
 
-if isdog:
-   print("Just a baby")
+if isdog == "true":
+   print("Just a baby")   #if variable, isdog, is true, then display "Just a baby"
 else:
-   print("not a baby")
+   print("not a baby")   #if variable is false, then print "not a baby"
 
 #functions with if
+
+
+isdog = input("Is a dog? ")  #prompts user to enter value for variable isdog
+
+if isdog == "True" or isdog == "true" or isdog == "yes" or isdog == "Yes":
+    print("Just a baby")  #lots of or statements that act as catchalls so user can enter multiple things and still make statement true
+else:
+    print("Not a baby")  # if user enters any other value this message will be displayed
 
