9aefd66bfafb2bdbddda32698e87d308d00e3625
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 20:22:26 -0400

    Update UserInput.py

--- UserInput.py
+++ UserInput.py
@@ -1,3 +1,6 @@
 name = input("What is your name? ")
 age = input("What is your age? ")
 print("Hello, " + name + "! " + "You are " + age)
+
+#another way to print above statement
+print(f"Hello, {name}! You are {age)!") 
