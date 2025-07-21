a3ede74e08398069c25c6f0a0402edc96a8550b4
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 20:20:19 -0400

    Update ReplaceLettersTranslator.py

--- ReplaceLettersTranslator.py
+++ ReplaceLettersTranslator.py
@@ -1,12 +1,13 @@
 def translate(phrase):    #defines a function called translate that requires one argument or parameter (phrase)
     translation = ""      #sets the translation to a blank string that we will build on
     for letter in phrase: #cycle through each character(letter) in the phrase
-        if letter in "AEIOUaeiou":  #checks if the current letter is a vowel
-            translation = translation + "g"  #if the letter is a vowel, it is replaced by a g and added to the empty translation string
-        else:   #if it is not a vowel
+        if letter in "AEIOUaeiou":  #checks if the current letter is a vowel. Boolean statement looks for true or false
+            translation = translation + "g"  #if the letter is a vowel and above is true, it is replaced by a g and added to the empty translation string
+        else:   #if it is not a vowel, if false
             translation = translation + letter   #add the original letter to the translation string
     return translation  #once the loop is finished, return the entire translation string
 
 print(translate(input("Enter a phrase: ")))  #prompts user to enter a custom phrase for the function parameter and prints that result after the translation
 
 
+#if user was to enter "cat" the output would show "cgt". The vowel has been replaced with a "g"
