744ec246480835ecc0369bc1d03666a26d43aec2
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-20 20:17:42 -0400

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,5 +1,5 @@
 friends = ['tom', 'mark', 'steve', 'joe']
-          # 0       1       2        3
+          # 0       1       2        3     index numbers start at 0 on left
 print(friends[0])
 
 #prints out tom 
@@ -8,15 +8,15 @@ print(friends[0])
 
 
 friends = ['tom', 'mark', 'steve', 'joe']
-          # 0       1       2        3
+          # 0       1       2        3     index numbers
 print(friends.index('steve'))
 
 #tells you what index number steve is located. In this case 2
 
 
 friends = ['tom', 'mark', 'steve', 'joe']
-          # 0       1       2        3
-friends.append('jim')
+          # 0       1       2        3    index numbers
+friends.append('jim')  #adds onto the end of the list
 print(friends)
 
 #add to the list
