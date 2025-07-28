ed480ebd95eb8869554d6898f84ae0af7a415192
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-07-28 10:50:06 -0400

    Create 99BottlesOfBeerForLoop.py

---
+++ 99BottlesOfBeerForLoop.py
@@ -0,0 +1,5 @@
+
+for i in range(99, 0, -1):  #Creating a for loop that starts at 99 and counts down to 0 going down by -1 everytime
+  
+  print(f"{i} bottles of beer on the wall\n{i} bottles of beer\ntake one down, and pass it around\n{i-1} bottles of beer.") 
+     #using an f string to include the variable {i} in the string. \n prints a new line. The last time {n-1} subtracts one from current value
