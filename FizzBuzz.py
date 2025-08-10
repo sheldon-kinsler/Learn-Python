cc92e5afe138fc5e8938845ddc8f8151cf3f0d11
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-08-10 17:38:55 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,7 +4,7 @@
 #For multiples of 5, print "Buzz" instead of the number.
 #Here's the tricky part: For multiples of 3 and 5, print "FizzBuzz".
 
-for i in range(1,101, 1):  #for each number in the range of 1-101 cunting up by 1
+for i in range(1,101, 1):  #for each number in the range of 1-101 counting up by 1
     if i % 3 == 0 and i % 5 == 0:  #the modulo operator, %, can find multiples of a number. If (num) % 3 == 0 it is a mutiple of 3 with 0 remainder
         print("FizzBuzz")          #9 % 3 == 0 as 3 goes into 9 perfectly. 10 % 3 == 1 because 3*3=9 and then 1 remainder to get to 10
     elif i % 3 == 0:
