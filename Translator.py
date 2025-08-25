0f411225a247b9b9b0e3607dd927e898798ca7e3
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-08-24 20:24:17 -0400

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,14 @@
+pip install translate #install the library for translating
+
+import translate
+from translate import Translator    #the specific library we need for this
+
+translator = Translator(to_lang='it')  #creating the translator object. 'it' is Italian, but can put any language you want, like 'es' for Spanish
+
+text = input('What would you like to translate? ')   #phrase to be translated. Having user complete this phrase
+
+translation = translator.translate(text)   #performing the translation using the correct library
+
+print('Translated Text: ', translation)    #print the translated text
+
+#output: 'Ciao, come stai?'
