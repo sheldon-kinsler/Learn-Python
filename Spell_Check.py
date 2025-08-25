b1df82f19747f5c183cbd5bb0ee9825f79fd8a52
Sheldon Kinsler <161544157+sheldon-kinsler@users.noreply.github.com>
2025-08-24 21:05:32 -0400

    Update Spell_Check.py

--- Spell_Check.py
+++ Spell_Check.py
@@ -1,14 +1,14 @@
-pip install textblob   #install correct library in terminal
+pip install textblob   #install correct library in terminal. A library that processes text
 
-import textblob
+import textblob   #call the library so python knows what library you want to use
 
-from textblob import TextBlob   #call the library
+from textblob import TextBlob   #imports the TextBlob class from the library that includes spell check
 
 text = input('Enter word or phrase to check for spelling: ')  #prompting user to enter word or phrase to spell check
 
-blob = TextBlob(text)   #creating a textblob object with the text
+blob = TextBlob(text)   #creating a textblob object with the text. blob now has all the features from the TextBlob class like correction
 
-corrected_text = blob.correct()  #creating the object that actually does the spell checking of the text
+corrected_text = blob.correct()  #creating the object that actually does the spell checking of the text. blob takes the place of TextBlob(text)
 
 print(f'Original Text: {text}')  #print the original entered text
 
